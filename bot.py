b4b9980d30e0a23f27e14658777ffc18182503d3
y_neko <96345521+nekoo1@users.noreply.github.com>
2022-06-20 19:24:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def mentionall(event):
         await client.send_message(chat_id, txt)
       elif mode == "text_on_reply":
         await msg.reply(usrtxt)
-      await asyncio.sleep(2)
+      await asyncio.sleep(0.5)
       usrnum = 0
       usrtxt = ''
   try:
